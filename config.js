25df3cf56d420e29bf84c53c0e2ce9af8b92e7e7
Amanda111 <631551773@qq.com>
2016-11-04 21:20:57 +0800

    add branch

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 module.exports = {
   author: {
-    name: "Joyee Cheung",
-    url: "https://github.com/joyeecheung"
+    name: "Amanda",
+    url: "https://github.com/Amanda111"
   },
   repo: {  // for the markdown source
-    content: "https://github.com/joyeecheung/my-tech-diary",
+    content: "https://github.com/Amanda111/my-tech-diary",
     as_submodule: 'diary'
   },
   base_url: '/diary',  // remote directory for the website
-  ga_id: 'UA-63818301-1',  // Optional: Universal Google Analytics ID
   title: 'My Tech Diary',
   lang: 'en'
 }
